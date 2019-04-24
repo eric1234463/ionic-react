c4174bd775e679c5bb3d59e440443c123e6d6c90
eric1234463 <eric1234463@gmail.com>
2019-04-24 22:27:25 +0800

    feat(ionic ): fix bugs

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { registerIonic } from '@ionic/react';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-registerIonic();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
